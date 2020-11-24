c8c0ad27319be51362fa6c38491448e62f9b2209
omarhajara <umarhajar3@gmail.com>
2020-11-23 17:30:14 -0800

    Modal-Update

--- src/Container/Card/card.js
+++ src/Container/Card/card.js
@@ -4,10 +4,23 @@ import Accredit from '../../Assets/accredit.png';
 import Appoint from '../../Assets/appoint.png';
 import Enrol from '../../Assets/enrol.png';
 import Sub from '../../Assets/sub.png';
+import Enrolment from '../Enrolment-Modal-Component/enrolment-modal'
+import Subsciption from '../Subscription-Modal-Component/subscription-modal'
+import Appointment from '../Appointment-Modal-Component/appointment-modal'
 
 
 class Card extends React.Component {
   
+  state = { show: false };
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
   render() {
     return (
       <div className="card">
@@ -41,7 +54,32 @@ class Card extends React.Component {
         </div>
 
         <div className="button">
-            <button>{this.props.buttonText}</button>
+          {
+                this.props.buttonText === "Enrol Today" ? (
+                  <div>
+                    <Enrolment show={this.state.show} handleClose={this.hideModal}/>
+                    <button onClick={this.showModal}>{this.props.buttonText}</button>
+                  </div>
+                ) : 
+                this.props.buttonText === "Apply Today" ? (
+                  <div>
+                    <a href='#'></a>
+                    <button onClick={this.showModal}>{this.props.buttonText}</button>
+                  </div>
+                ) : 
+                this.props.buttonText === "Pay Subscription" ? (
+                  <div>
+                    <Subsciption show={this.state.show} handleClose={this.hideModal}/>
+                    <button onClick={this.showModal}>{this.props.buttonText}</button>
+                  </div>
+                ) : 
+                this.props.buttonText === "Book Appointment" ? (
+                  <div>
+                    <Appointment show={this.state.show} handleClose={this.hideModal}/>
+                    <button onClick={this.showModal}>{this.props.buttonText}</button>
+                  </div>
+                ) :null  
+              }
         </div>
       </div>
     );
--- src/Container/Enrolment-Modal-Component/enrolment-modal.js
+++ src/Container/Enrolment-Modal-Component/enrolment-modal.js
@@ -1,34 +1,43 @@
 import React from 'react';
-import './enrolment-modal.css';
+import './enrolment-modal.css'
 import infoLogo from '../../Assets/info-logo.png';
 
 
 
 
+const Enrolment= ({ handleClose, show }) => {
+    const showHideClassName = show ? "modal display-block" : "modal display-none";
 
-class Enrolment extends React.Component {
-    render() {
-        return (
-            <div class='container'>
-                <div class='card'>
+ return (
+    <div className={showHideClassName}>
+                   <div class='container enrolment'>
+                <div class='card enrolment'>
                     <div class='card-header'>
                         <p>Enrolment information</p>
-                        <img src={infoLogo} alt="" />
+                        <img src= {infoLogo} />
                     </div>
                     <div class='card-detail'>
                         <p>Please meet our team member
-                        in your Ward to enrol.
-                            Call 0700PLATEAU for <br />
+                            in your Ward to enrol.
+                            Call 0700PLATEAU for <br/>
                             more information
                         </p>
-                        <button>OK</button>
+                        <button className="ui button" onClick={handleClose}>OK</button>
                     </div>
 
                 </div>
             </div>
-        );
-    }
-}
+
+    </div>
+
+                       
+        
+     
+);
+};
+
+
+
 
 
 export default Enrolment;
